4463379f69a1bf0834ac5efa5974819ec0c40650
Shiju Reji <shijureji5@gmail.com>
2026-01-16 18:14:26 +0530

    firebase initilize

---
+++ lib/app_router.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'presentation/bloc/auth_bloc.dart';
+import 'presentation/bloc/auth_state.dart';
+import 'presentation/pages/home_page.dart';
+import 'presentation/pages/login_page.dart';
+
+class AppRouter {
+  final AuthBloc authBloc;
+
+  AppRouter(this.authBloc);
+
+  GoRouter get router => GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return BlocBuilder<AuthBloc, AuthState>(
+            bloc: authBloc,
+            builder: (context, authState) {
+              if (authState is AuthLoggedIn) {
+                return const HomePage();
+              } else {
+                return const LoginPage();
+              }
+            },
+          );
+        },
+      ),
+    ],
+  );
+}
---
+++ lib/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'auth_event.dart';
+import 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthBloc({FirebaseAuth? firebaseAuth})
+    : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+      super(AuthInitial()) {
+    on<AppStarted>(_onAppStarted);
+  }
+
+  Future<void> _onAppStarted(AppStarted event, Emitter<AuthState> emit) async {
+    final user = _firebaseAuth.currentUser;
+    if (user != null) {
+      emit(AuthLoggedIn());
+    } else {
+      emit(AuthLoggedOut());
+    }
+  }
+}
